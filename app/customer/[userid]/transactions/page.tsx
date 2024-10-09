ac137a3b5fcf53f28cf1757362e0d68c08b39178
victorkimathi <victorcodes9532@gmail.com>
2024-10-09 07:09:44 +0300

    made transactions page

---
+++ app/customer/[userid]/transactions/page.tsx
@@ -0,0 +1,166 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { Form, FormControl } from "@/components/ui/form";
+import { Label } from "@/components/ui/label";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+import { SelectItem } from "@/components/ui/select";
+import { dummyAccount } from "@/constants";
+import { GenderOptions,CategoryOptions,IncomeSubCategoryOptions,ExpenseSubCategoryOptions CustomerFormDefaultValues } from "@/constants";
+import { CustomerFormValidation } from "@/lib/vallidation";
+import "react-datepicker/dist/react-datepicker.css";
+import "react-phone-number-input/style.css";
+import CustomFormField, { FormFieldType } from "@/components/CustomFormField";
+import SubmitButton from "@/components/SubmitButton";
+
+const RegisterForm = ({ user }: { user: User }) => {
+    const router = useRouter();
+    const [isLoading, setIsLoading] = useState(false);
+
+    const form = useForm<z.infer<typeof CustomerFormValidation>>({
+        resolver: zodResolver(CustomerFormValidation),
+        defaultValues: {
+            ...CustomerFormDefaultValues,
+            name: user.name,
+            email: user.email,
+            phone: user.phone,
+        },
+    });
+
+    const onSubmit = async (values: z.infer<typeof CustomerFormValidation>) => {
+        setIsLoading(true);
+
+
+
+
+
+        try {
+            const customer = {
+                userId: user.$id,
+                name: values.name,
+                email: values.email,
+                phone: values.phone,
+                birthDate: new Date(values.birthDate),
+                gender: values.gender,
+                address: values.address,
+                occupation: values.occupation,
+                emergencyContactName: values.emergencyContactName,
+                emergencyContactNumber: values.emergencyContactNumber,
+                accountProviders: values.accountProviders,
+                savingAmount: values.savingAmount,
+                debt: values.debt,
+                income: values.income,
+                description: values.description,
+                subscriptionInformation: values.subscriptionInformation,
+                identificationType: values.identificationType,
+                identificationNumber: values.identificationNumber,
+                identificationDocument: values.identificationDocument
+                    ? formData
+                    : undefined,
+                financialAdviceConsent: values.financialAdviceConsent,
+                dataSharingConsent: values.dataSharingConsent,
+                privacyPolicyConsent: values.privacyPolicyConsent,
+                automatedDecisionConsent: values.automatedDecisionConsent,
+            };
+
+            // const newCustomer = await registerCustomer(customer);
+            console.log("Hello")
+            console.log(customer);
+            if(customer){
+
+                router.replace(`/customer/${123}/success`);
+            }
+
+        } catch (error) {
+            console.error(error);
+        }
+
+        setIsLoading(false);
+    };
+
+    return (
+        <Form {...form}>
+            <form
+                onSubmit={form.handleSubmit(onSubmit)}
+                className="flex-1 space-y-12"
+            >
+                <section className="space-y-4">
+                    <h1 className="header">Welcome 👋</h1>
+                    <p className="text-dark-700">Enter your transactions.</p>
+                </section>
+                <CustomFormField
+                    fieldType={FormFieldType.TEXTAREA}
+                    control={form.control}
+                    name="transactionName"
+                    label="Enter the transaction Name"
+                    placeholder="I bought Food"
+                />
+
+                <CustomFormField
+                    fieldType={FormFieldType.SKELETON}
+                    control={form.control}
+                    name="category"
+                    label="Income/Expense"
+                    renderSkeleton={(field) => (
+                        <FormControl>
+                            <RadioGroup
+                                className="flex h-11 gap-6 xl:justify-between"
+                                onValueChange={field.onChange}
+                                defaultValue={field.value}
+                            >
+                                {CategoryOptions.map((option, i) => (
+                                    <div key={option + i} className="radio-group">
+                                        <RadioGroupItem value={option} id={option}/>
+                                        <Label htmlFor={option} className="cursor-pointer">
+                                            {option}
+                                        </Label>
+                                    </div>
+                                ))}
+
+                                // I want to have Logic that will check what category wwas selected and based on those categories map either income subcategory or expense subcategories as select
+                            </RadioGroup>
+                        </FormControl>
+                    )}
+                />
+
+
+
+                    <h2>Select Your Account Providers</h2>
+                    {dummyAccount.map((provider) => (
+                        <CustomFormField
+                            key={provider}
+                            fieldType={FormFieldType.CHECKBOX}
+                            control={form.control}
+                            name={provider}
+                            label={provider}
+                        />
+                    ))}
+
+                    <div className="flex flex-col gap-6 xl:flex-row">
+                        <CustomFormField
+                            fieldType={FormFieldType.INPUT}
+                            control={form.control}
+                            name="Transaction Amount"
+                            label="Transaction Or expense AMount "
+                            placeholder="500 000"
+                        />
+                        <CustomFormField
+                            fieldType={FormFieldType.TEXTAREA}
+                            control={form.control}
+                            name="transactionDescription"
+                            label="Describe the transaction you just made "
+                            placeholder="I sent money from mama Mboga /I received my salary"
+                        />
+                    </div>
+                <SubmitButton isLoading={isLoading}>Submit and Continue</SubmitButton>
+            </form>
+        </Form>
+    );
+};
+
+export default RegisterForm;
